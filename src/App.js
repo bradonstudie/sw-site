d8bba01b454d766ca35b4874a3341a12e423f5cd
Bradon <bradon@Bradons-MacBook-Pro.local>
2021-04-24 20:33:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import  { Button, Container, Row, Col, Image } from "react-bootstrap";
+import logo from "./assets/img/sw-logo.gif";
+import alien from "./assets/img/alien.gif";
+import time from "./assets/img/420time.gif";
+import space from "./assets/img/space.jpeg";
+import alienop from "./assets/img/alienop.gif";
 
 function App() {
+  const appStyle = {
+    backgroundImage: `url(${space})`,
+    backgroundRepeat: "repeat",
+    minHeight: "100vh",
+    fontFamily: "serif"
+  }
+
+  const imageStyle = {
+    width: "15rem"
+  };
+
+  const devotedStyle = {
+    color: "lawngreen",
+    fontWeight: "bold"
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={ appStyle }>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row>
+            <Col sm={ 12 } className="d-flex align-items-between justify-content-center">
+              <Image src={ alienop } fluid style={ imageStyle } />
+              <Image src={ logo } fluid style={ imageStyle } />
+              <Image src={ alienop } fluid style={ imageStyle } />
+            </Col>
+            <Col sm={ 12 } className="d-flex flex-column align-items-center justify-content-center">
+              <p style={ devotedStyle }>Are you devoted?</p>
+              <Image src={ alien } fluid style={ imageStyle } />
+            </Col>
+            <Col sm={ 12 } className="d-flex align-items-center justify-content-center">
+              <Image src={ time } fluid style={ imageStyle } />
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
